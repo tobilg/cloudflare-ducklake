3dae887ac8c6fed06e157f26c8585be696a09fb2
Jason Baker <jason.th.baker@gmail.com>
2025-06-30 11:18:14 -0500

    add spatial extension

--- scripts/download_extensions.sh
+++ scripts/download_extensions.sh
@@ -5,6 +5,7 @@ mkdir -p $PWD/extensions
 
 # Download extensions
 curl https://extensions.duckdb.org/v1.3.1/linux_amd64/httpfs.duckdb_extension.gz --output $PWD/extensions/httpfs.duckdb_extension.gz
+curl https://extensions.duckdb.org/v1.3.1/linux_amd64/spatial.duckdb_extension.gz --output $PWD/extensions/spatial.duckdb_extension.gz
 curl https://extensions.duckdb.org/v1.3.1/linux_amd64/iceberg.duckdb_extension.gz --output $PWD/extensions/iceberg.duckdb_extension.gz
 curl https://extensions.duckdb.org/v1.3.1/linux_amd64/ducklake.duckdb_extension.gz --output $PWD/extensions/ducklake.duckdb_extension.gz
 curl https://extensions.duckdb.org/v1.3.1/linux_amd64/postgres_scanner.duckdb_extension.gz -o $PWD/extensions/postgres_scanner.duckdb_extension.gz
@@ -12,6 +13,7 @@ curl https://community-extensions.duckdb.org/v1.3.1/linux_amd64/nanoarrow.duckdb
 
 # Unzip 
 gunzip $PWD/extensions/httpfs.duckdb_extension.gz
+gunzip $PWD/extensions/spatial.duckdb_extension.gz
 gunzip $PWD/extensions/iceberg.duckdb_extension.gz
 gunzip $PWD/extensions/ducklake.duckdb_extension.gz
 gunzip $PWD/extensions/postgres_scanner.duckdb_extension.gz

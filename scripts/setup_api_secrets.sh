1083ef6f7ea74da3726a8faccf6e24bcaf95d4fb
Jason Baker <jason.th.baker@gmail.com>
2025-06-30 11:41:58 -0500

    validation of token, add documention in readme

--- scripts/setup_api_secrets.sh
+++ scripts/setup_api_secrets.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+echo -n "Enter API_TOKEN: "
+read -r API_TOKEN
 
-# Create a secret for the API token
 echo "Validating API_TOKEN per https://hono.dev/docs/middleware/builtin/bearer-auth"
-
+# Validate against the regex
 if [[ ! "$API_TOKEN" =~ ^[A-Za-z0-9._~+/-]+=*$ ]]; then
   echo "Error: API_TOKEN does not match the required pattern of /[A-Za-z0-9._~+/-]+=*/!" >&2
   exit 1
 fi
-echo "Now setting up API_TOKEN..."
-wrangler secret put API_TOKEN
+
+echo "Now setting up API_TOKEN as a Wrangler secret..."
+# Pipe the validated value to wrangler
+echo "$API_TOKEN" | wrangler secret put API_TOKEN
